dd6953ec9accbb4ef2c8edbcc4ed4b4f052fa247
Jon Fuller <jonfuller0207@gmail.com>
2024-05-12 18:07:24 +0100

    feat: combine adjacent messages with the same author

--- src/data_parser.py
+++ src/data_parser.py
@@ -8,9 +8,13 @@ class ChatParser:
         self.datetime_format = "%d/%m/%Y, %H:%M"
         self.dateTimeRegex = "^\d{2}/\d{2}/\d{4}\,\s+\d{2}:\d{2}"
         self.nameRegex = "^[^:]+"
+        
+        self.nonStandardRows = []
+
         self.raw_data = self.load_data()
         self._chat = self.raw_data
-        self.nonStandardRows = []
+        self.parsed_data = self.parse_data()
+        
 
     def load_data(self):
         with open(self.filepath, "r") as file:
@@ -54,11 +58,34 @@ class ChatParser:
         names = self.extract_names()
 
         return {
-            row: [dateTimes[row], names[row], self._chat[row]]
+            row: {"date": dateTimes[row], "author": names[row], "text": self._chat[row]}
             for row in range(0, len(self._chat))
         }
+    
+    def merge_messages(self):
+        """Method to merge together messages from the same author. Seperate messages are concatenated with an <eom> (end of message) marker"""
+        endOfNewAuthors = None
+        combinedReplyI = 0
+        combinedMessages = {}
 
+        for key, value in self.parsed_data.items():
+            if (endOfNewAuthors is None) or (key > endOfNewAuthors):
+                if key < len(self.parsed_data) - 1:
+                    # work out how many more message consequetive messages are by the same author
+                    consequetiveAuthors = []
+                    for i in range(key, len(self.parsed_data)):
+                        if self.parsed_data[i]["author"] == value["author"]:
+                            consequetiveAuthors.append(i)
+                        else:
+                            break
+                    endOfNewAuthors = (
+                        max(consequetiveAuthors) if len(consequetiveAuthors) > 0 else i
+                    )
 
-if __name__ == "__main__":
-    Chat = ChatParser("data/WhatsApp Chat with Izzie Fuller.txt")
-    print(Chat.parse_data())
+                    combined_text = " <eom> ".join(
+                        [self.parsed_data[i]["text"] for i in range(key, endOfNewAuthors + 1)]
+                    ) + " <eom>"
+                    combinedMessages[combinedReplyI] = {value["author"]: combined_text}
+                    combinedReplyI = combinedReplyI + 1
+        
+        return combinedMessages
