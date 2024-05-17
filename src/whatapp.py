b3ae0124fb93d82ed337fc4290a9fce348676883
Jon Fuller <jonfuller0207@gmail.com>
2024-05-17 16:19:18 +0100

    docs: include ChatParser docstrings

--- src/whatapp.py
+++ src/whatapp.py
@@ -3,7 +3,26 @@ from datetime import datetime
 
 
 class ChatParser:
+    """
+    A class to parse Whatsapp chat logs, as .txts, and extract useful information including timestamps, authors, and messages.
+
+    Attributes:
+        filepath (str): The path to the Whatsapp chat .txt file.
+        datetime_format (str): The format of the datetime strings in the Whatsapp chat log.
+        dateTimeRegex (str): The regex pattern to identify datetime strings in the Whatsapp chat log.
+        nameRegex (str): The regex pattern to identify author names in the Whatsapp chat log.
+        nonStandardRows (list): A list to store row indices for rows that cannot be formated.
+        raw_data (list): The raw lines of data loaded from the Whatsapp chat log file.
+        parsed_data (dict): A dictionary containing parsed data with datetime, author, and text.
+    """
+        
     def __init__(self, filepath: str) -> None:
+        """
+        Initialises the ChatParser with the specified file path.
+
+        Args:
+            filepath (str): The path to the Whatsapp chat .txt file.
+        """
         self.filepath = filepath
         self.datetime_format = "%d/%m/%Y, %H:%M"
         self.dateTimeRegex = "^\d{2}/\d{2}/\d{4}\,\s+\d{2}:\d{2}"
@@ -17,6 +36,12 @@ class ChatParser:
         
 
     def load_data(self):
+        """
+        Loads the chat log data from the file, skipping the first line as is a useless line.
+
+        Returns:
+            list: A list of Whatsapp chat lines, excluding the first line as is a useless line.
+        """
         with open(self.filepath, "r") as file:
             return [line for i, line in enumerate(file) if i > 0]
 
@@ -39,6 +64,12 @@ class ChatParser:
         return messageDateTimes
 
     def extract_names(self):
+        """
+        Extracts author names from the chat messages.
+
+        Returns:
+            list: A list of author names corresponding to each message.
+        """
         messageNames = []
         for row, message in enumerate(self._chat):
             if row not in self.nonStandardRows:
@@ -54,6 +85,12 @@ class ChatParser:
         return messageNames
 
     def parse_data(self):
+        """
+        Parses the chat data to extract datetime, author, and text for each message.
+
+        Returns:
+            dict: A dictionary with row indices as keys and dictionaries containing 'date', 'author', and 'text' as values.
+        """
         dateTimes = self.extract_datetimes()
         names = self.extract_names()
 
@@ -63,7 +100,12 @@ class ChatParser:
         }
     
     def merge_messages(self):
-        """Method to merge together messages from the same author. Seperate messages are concatenated with an <eom> (end of message) marker"""
+        """
+        Merges consecutive messages from the same author into a single message, concatenated with an <eom> (end of message) marker.
+
+        Returns:
+            dict: A dictionary with merged messages, using the author as the key and the combined text as the value.
+        """
         endOfNewAuthors = None
         combinedReplyI = 0
         combinedMessages = {}
